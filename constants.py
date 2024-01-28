8ae4e56e5c453f9621bb1c6269b0cbeb81d40ec3
sergnator <86975413+sergnator@users.noreply.github.com>
2024-01-28 19:35:00 +0300

    4.0

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-VERSION = '%(prog)s 3.7'
+VERSION = '%(prog)s 4.0'
 HREF = 'https://api.github.com/repos/sergnator/utilisexe/branches/exeBranch'
 HREF_DOWNLOAD = 'https://raw.githubusercontent.com/sergnator/utilisexe/exeBranch/dist/main.exe'
